40571cafc6e892db0dfdda490bb3ef187ad4c57e
Benedikt D Valdez <benediktvaldez@gmail.com>
2017-05-30 14:35:29 +0000

    Initial commit with cra, prettier and assets

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// Disable service workers for now
+// import registerServiceWorker from './registerServiceWorker'
+// registerServiceWorker()
